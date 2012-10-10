070de196d8b430f7e4b3112c154f459240ff5728
klucha-leniwa <klucha.leniwa@gmail.com>
2012-10-10 14:00:28 +0200

    urls fix

--- deedee/my_site/urls.py
+++ deedee/my_site/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('my_site.views',
     url(r'^index/$', 'index'),
     url(r'^$', 'index'),
-    url(r'^base/$', 'base'),
     url(r'posts/(?P<post_id>\d+)/$', 'details'),
     url(r'^category/(?P<category_name>\w+)/$', 'category'),
-    url(r'^form/$', 'manage_comment')
 )
 
 urlpatterns += staticfiles_urlpatterns()
